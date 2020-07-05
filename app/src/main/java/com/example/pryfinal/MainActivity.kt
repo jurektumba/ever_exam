d27d3b6800f1b7123c61998558999846eee3fd2a
Jurek <camilotb94_1@hotmail.com>
2020-07-05 12:49:51 -0500

    test2

--- app/src/main/java/com/example/pryfinal/MainActivity.kt
+++ app/src/main/java/com/example/pryfinal/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
     }
-//este es el proyecto limpio para el examen?, efectivamente
+//este es el proyecto limpio para el examen?, efectivamente este es el proyecto
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
