d6383713e957c21fc03be66c2170a3a37c604e1a
Jurek <camilotb94_1@hotmail.com>
2020-07-05 16:56:42 -0500

    Diseño de FrmUserProfile

---
+++ app/src/main/java/com/example/pryfinal/FrmUserPostsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pryfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrmUserPostsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frm_user_posts)
+    }
+}
---
+++ app/src/main/java/com/example/pryfinal/FrmUserProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pryfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrmUserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frm_user_profile)
+    }
+}
